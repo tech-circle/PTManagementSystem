37cb9fcdb59c35e828877534bbf5f3a3fdc0d2a5
Julien D <julien@duraj.fr>
2016-02-21 18:12:56 +0000

    flow right menu

--- pt_system/web/elm/Menu.elm
+++ pt_system/web/elm/Menu.elm
@@ -1,7 +1,17 @@
 module Menu (..) where
 
 import Html
+import Graphics.Element exposing (..)
 
 
+main : Element
 main =
-  Html.text "Hello World"
+  flow right (List.map (width 150) content)
+
+
+content : List Element
+content =
+  [ show "item 1"
+  , show "item 2"
+  , show "item 3"
+  ]
